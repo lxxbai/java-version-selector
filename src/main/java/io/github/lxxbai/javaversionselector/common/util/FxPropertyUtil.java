c6e1c86fbd6eee12c4804ddf6ae269eab199166a
wdc <shupeng_liao@gmail.com>
2024-10-21 22:14:50 +0800

    add

--- src/main/java/io/github/lxxbai/javaversionselector/common/util/FxPropertyUtil.java
+++ src/main/java/io/github/lxxbai/javaversionselector/common/util/FxPropertyUtil.java
@@ -1,30 +1,37 @@
 package io.github.lxxbai.javaversionselector.common.util;
 
-import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
-import io.github.lxxbai.javaversionselector.common.exception.ClientException;
 import javafx.beans.property.*;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Date;
+import java.util.Objects;
 
 public class FxPropertyUtil {
 
 
-    public static Property getProperty(Class fieldType, Object value) {
-        if (String.class == fieldType || Date.class == fieldType || BigInteger.class == fieldType) {
-            return new SimpleStringProperty(StrUtil.toStringOrNull(value));
-        } else if (BigDecimal.class == fieldType || (Double.TYPE == fieldType) || (Double.class == fieldType)) {
-            return new SimpleDoubleProperty();
-        } else if ((Integer.TYPE == fieldType) || (Integer.class == fieldType)) {
-            return new SimpleIntegerProperty();
-        } else if ((Long.TYPE == fieldType) || (Long.class == fieldType)) {
-            return new SimpleLongProperty();
-        } else if ((Float.TYPE == fieldType) || (Float.class == fieldType)) {
-            return new SimpleFloatProperty();
-        } else {
-            throw new ClientException("未适配的属性");
+    public static <T> Property<T> getProperty(Class<T> fieldType, T value) {
+        if (String.class == fieldType) {
+            return (Property<T>) new SimpleStringProperty(StrUtil.toStringOrNull(value));
         }
+        if ((Integer.TYPE == fieldType) || (Integer.class == fieldType)) {
+            return (Property<T>) (Objects.isNull(value) ? new SimpleIntegerProperty() :
+                                new SimpleIntegerProperty((Integer) value));
+        }
+        if ((Double.TYPE == fieldType) || (Double.class == fieldType)) {
+            return (Property<T>) (Objects.isNull(value) ? new SimpleDoubleProperty() :
+                                new SimpleDoubleProperty((Double) value));
+        }
+        if ((Long.TYPE == fieldType) || (Long.class == fieldType)) {
+            return (Property<T>) (Objects.isNull(value) ? new SimpleLongProperty() :
+                                new SimpleLongProperty((Long) value));
+        }
+        if ((Float.TYPE == fieldType) || (Float.class == fieldType)) {
+            return (Property<T>) (Objects.isNull(value) ? new SimpleFloatProperty() :
+                                new SimpleFloatProperty((Float) value));
+        }
+        if ((Boolean.TYPE == fieldType) || (Boolean.class == fieldType)) {
+            return (Property<T>) (Objects.isNull(value) ? new SimpleBooleanProperty() :
+                                new SimpleBooleanProperty((Boolean) value));
+        }
+        return Objects.isNull(value) ? new SimpleObjectProperty<>() : new SimpleObjectProperty<>(value);
     }
 }
--- src/main/java/io/github/lxxbai/javaversionselector/component/ModelProperty.java
+++ src/main/java/io/github/lxxbai/javaversionselector/component/ModelProperty.java
@@ -9,6 +9,7 @@ import javafx.beans.property.Property;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class ModelProperty<T> {
 
@@ -16,29 +17,43 @@ public class ModelProperty<T> {
 
     private Class<T> clazz;
 
-    private Map<Field, Property<?>> propertyMap = new HashMap<>();
+    private final Map<Field, Property<?>> propertyMap = new HashMap<>();
 
-    public ModelProperty(Class<T> clazz) {
+    public ModelProperty() {
+    }
 
+    public ModelProperty(T model) {
+        this.model = model;
+        this.clazz = (Class<T>) model.getClass();
     }
 
 
     public T getModel() {
-
+        if (propertyMap.isEmpty()) {
+            return model;
+        }
+        if (Objects.isNull(model)) {
+            model = ReflectUtil.newInstance(clazz);
+        }
+        propertyMap.forEach((field, property) ->
+                ReflectUtil.setFieldValue(model, field, property.getValue()));
         return model;
     }
 
-    public <P> Property<P> buildProperty(Func1<T, ?> function) {
+    public <P> Property<P> buildProperty(Func1<T, P> function) {
         // 获取属性类型
         String fieldName = LambdaUtil.getFieldName(function);
+        if (Objects.isNull(clazz)) {
+            clazz = LambdaUtil.getRealClass(function);
+        }
         //获取字段
         Field field = ReflectUtil.getField(clazz, fieldName);
         //获取属性值
-        Object fieldValue = ReflectUtil.getFieldValue(model, field);
+        P fieldValue = Objects.isNull(model) ? null : (P) ReflectUtil.getFieldValue(model, field);
         // 获取属性类型
-        Class<?> type = field.getType();
+        Class<P> type = (Class<P>) field.getType();
         //创建属性
-        Property property = FxPropertyUtil.getProperty(type, fieldValue);
+        Property<P> property = FxPropertyUtil.getProperty(type, fieldValue);
         propertyMap.put(field, property);
         return property;
     }
